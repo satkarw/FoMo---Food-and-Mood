32d34b68b3f89a81cb10607d209d874f31c728f3
satkarw <satkarw@gmail.com>
2026-01-20 18:04:42 +0545

    more errors

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Run database migrations
-python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --no-input
